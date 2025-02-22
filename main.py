94030fc1893b32716e7cc9f21662ad9c140642a7
Shreekar Dittakavi <shreksodyne414@gmail.com>
2025-02-22 17:07:54 -0600

    Added routes

--- main.py
+++ main.py
@@ -3,9 +3,46 @@ import db
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('auth/login.html')
+
+
+@app.route('/register/')
+def register():
+    return render_template('auth/register.html')
+
+
+@app.route('/setup/')
+def setup():
+    return render_template('auth/setup.html')
+
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/update/')
+def update():
+    return render_template('update.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=1234)
