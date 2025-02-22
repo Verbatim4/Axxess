b76113f739bb91e73c5450620bc5e8baafc4beab
Shreekar Dittakavi <shreksodyne414@gmail.com>
2025-02-22 17:01:28 -0600

    Added database

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
+import db
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=1234)
