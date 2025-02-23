2ac896d5ea002d66a01cccbcdb827644c4bfccef
Shelley <scn220006@utdallas.edu>
2025-02-23 03:32:54 -0600

    a

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
 
 @app.route('/login/')
 def login():
